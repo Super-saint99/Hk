bc6b418bfc12f82fc7a6ef14c52188d49c96309f
Super-saint99 <harieff123@gmail.com>
2025-08-15 22:30:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Replace with your own Google Web App URL
-const GOOGLE_WEB_APP_URL = "PASTE_YOUR_WEB_APP_URL_HERE";
+const GOOGLE_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw0AdaEE6DFJWMCZDLwEO6ob6nbWN0AS9JPfRmeGoRGvJuS4USiTmbbQBefTsbwL5j7tQ/exec";
 // Admin credentials
 const ADMIN_USERNAME = "admin123";
 const ADMIN_PASSWORD = "pass123";
