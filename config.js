975ffe2a75fdf04ecce3f0319b8df5b93da1a0b6
Super-saint99 <harieff123@gmail.com>
2025-08-15 22:18:24 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Replace with your own Google Web App URL
-const GOOGLE_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzXnVlh2070bgFoEFwr8MX_YTNOBPgdlU37N0akP6JCsH5gJ1qA2ktTvbrsrDzAgEo9KA/exec";
-// Admin credentials
-const ADMIN_USERNAME = "admin123";
-const ADMIN_PASSWORD = "pass123";
