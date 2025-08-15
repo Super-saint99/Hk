e8d073e5fc92f2652f2615e2b91b94ec86db0eb1
Super-saint99 <harieff123@gmail.com>
2025-08-15 22:05:28 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Replace with your own Google Web App URL
+const GOOGLE_WEB_APP_URL = "PASTE_YOUR_WEB_APP_URL_HERE";
+// Admin credentials
+const ADMIN_USERNAME = "admin123";
+const ADMIN_PASSWORD = "pass123";
